ea195e73ae5a6b05238482f401d9ad863fc1fa32
Geovanni Sanchez <lalho.sanchez@gmail.com>
2022-06-27 20:47:18 -0500

    Ejemplos hasta la clase 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GeneradoCliComponent } from './prueba-componentes/generado-cli/generado-cli.component';
+import { GeneradoManoComponent } from './prueba-componentes/generado-mano/generado-mano.component';
+import { MiDirectivaDirective } from './directivas/mi-directiva.directive';
+import { FormsModule } from '@angular/forms';
+import { HijoComponent } from './clase-5/hijo/hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneradoCliComponent,
+    GeneradoManoComponent,
+    MiDirectivaDirective,
+    HijoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
